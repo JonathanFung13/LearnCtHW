a1389ab7f570c494a5536ecf652768bbf77fc72f
Jonathan Fung <jfung9@gatech.edu>
2019-07-31 18:34:03 -0700

    Arrays and strings complete.

--- ex11.c
+++ ex11.c
@@ -1,50 +1,33 @@
-#include <stdio.h>
+#include "stdio.h"
 
 int main(int argc, char *argv[])
 {
-	int numbers[4] = { 2 };
-	char name[4] = { 'a' }; // never do it this way
-	
-	// first, print them out raw
-	printf("numbers: %d %d %d %d\n",
-			numbers[0], numbers[1], numbers[2], numbers[3]);
+    int numbers[4] = {0};
+    char name[4] = {'a'};
 
-	printf("name each: %c %c %c %c\n",
-			name[0], name[1], name[2], name[3]);
+    // print raw
+    printf("numbers: %d %d %d %d \n", numbers[0], numbers[1], numbers[2], numbers[3]);
+    printf("names: %c %c %c %c \n", name[0], name[1], name[2], name[3]);
+    printf("name: %s \n", name);
 
-	printf("name: %s\n", name);
+    //setup the name and numbers
+    numbers[0] = 0;
+    numbers[1] = 1;
+    numbers[2] = 2;
+    numbers[3] = 3;
+    name[0] = 'J';
+    name[1] = 'o';
+    name[2] = 'n';
+    name[3] = '\0';
 
-	// setup the numbers
-	numbers[0] = 1;
-	numbers[1] = 2;
-	numbers[2] = 3;
-	numbers[3] = 4;
+    // print again
+    printf("numbers: %d %d %d %d \n", numbers[0], numbers[1], numbers[2], numbers[3]);
+    printf("names: %c %c %c %c \n", name[0], name[1], name[2], name[3]);
+    printf("name: %s \n", name);
 
-	// setup the name
-	name[0] = 'J';
-	name[1] = 'o';
-	name[2] = 'n';
-	name[3] = '\0';
-	
-	// then print them out initialized
-	printf("numbers: %d %d %d %d\n",
-			numbers[0], numbers[1], numbers[2], numbers[3]);
+    char *another = "Jonathan";
+    printf("another %s\n", another);
+    printf("another each: %c %c %c %c\n", another[0], another[1], another[2], another[3]);
 
-	printf("name each: %c %c %c %c\n",
-			name[0], name[1], name[2], name[3]);
-
-	// print the name like a string
-	printf("name: %s\n", name);
-
-	// another way to use name
-	char *another = "Jon";  // assign strings this way
-
-	printf("another: %s\n", another);
-
-	printf("another each: %c %c %c %c\n",
-			another[0], another[1], another[2], another[3]);
-
-	return 0;
+    return 0;
 }
-
-
