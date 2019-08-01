bd880b00cceeb6893af8fee840c90b4c3bd410a1
Jonathan Fung <jfung9@gatech.edu>
2019-07-31 19:38:49 -0700

    Pointers.

--- ex15.c
+++ ex15.c
@@ -1,48 +1,36 @@
-#include <stdio.h>
+#include "stdio.h"
 
 int main(int argc, char *argv[])
 {
-	// create two arrays we care about
-	int ages[] = { 23, 43, 12, 89, 2 };
-	char *names[] = {
-		"Alan", "Frank",
-		"Mary", "John", "Lisa"
-	};
+    int ages[] = {12, 23, 42, 55, 67};
+    char *names[] = {"Billy", "Cathy", "Donald", "Eric", "Frank"};
 
-	// safely get the size of ages
-	int count = sizeof(ages) / sizeof(int);
-	int i = 0;
+    int count = sizeof(ages) / sizeof(int);
+    int i = 0;
 
-	// first was using indexing
-	for (i = 0; i < count; i++) {
-		printf("%s has %d years alive.\n", names[i], ages[i]);
-	}
+    // first way of printing, same way as before
+    for (i = 0; i < count; i++) {
+        printf("%s is %d years young.\n", names[i], ages[i]);
+    }
+    printf("---\n");
 
-	printf("---\n");
+    // use pointers
+    int *cur_age = ages;
+    char **cur_name = names;
+    for (i = 0; i < count; i++) {
+        printf("%s is %d years young.\n", *(cur_name + i), *(cur_age + i));
+    }
+    printf("---\n");
 
-	// setup the pointers to the start of the arrays
-	int *cur_age = ages;
-	char **cur_name = names;
+    // another way to use pointers, array-like syntax
+    for (i = 0; i < count; i++) {
+        printf("%s is %d years young.\n", cur_name[i], cur_age[i]);
+    }
+    printf("---\n");
 
-	// second way using pointers
-	for (i = 0; i < count; i++) {
-		printf("%s is %d years old.\n", *(cur_name + i), *(cur_age + i));
-	}
-
-	printf("---\n");
-
-	// third way, pointers are just arrays
-	for (i = 0; i < count; i++) {
-		printf("%s is %d years old again.\n", cur_name[i], cur_age[i]);
-	}
-
-	printf("---\n");
-
-	// fourth way with pointers in a stupid complex way
-	for (cur_name = names, cur_age = ages;
-			(cur_age - ages) < count; cur_name++, cur_age++) {
-		printf("%s lived %d years so far.\n", *cur_name, *cur_age);
-	}
-
-	return 0;
+    // complex way to use pointers
+    for (cur_age=ages, cur_name=names; (cur_age-ages)<count; cur_age++, cur_name++) {
+        printf("%s is %d years young.\n", *cur_name, *cur_age);
+    }
+    return 0;
 }
